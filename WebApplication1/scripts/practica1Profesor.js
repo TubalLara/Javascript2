e6faf56c9de216ad2c007d07d7fe1bda7ed4cc39
TubalLara <tublargar@gmail.com>
2014-10-01 14:09:24 +0200

    practicaMiayProfesor

---
+++ WebApplication1/scripts/practica1Profesor.js
@@ -0,0 +1,35 @@
+﻿var suma = function (datos) {
+    var suma = 0;
+    for (var i = 0; i < datos.lenght; i++) {
+        suma += parseInt(datos[i])
+
+    }
+
+    return suma;
+};
+
+var media = function (datos) {
+    var total = suma(datos);
+
+    this.media = total / datos.lenght;
+};
+
+var ver = function (obj) {
+    alert(obj.media);
+
+};
+
+(function() {
+    var datos = [];
+    while (true) {
+        var n = prompt("Dime un numero");
+        if(isNaN(n))
+            break;
+
+        datos.push(n)
+    }
+    var obj = {};
+    alert(suma(datos));
+    media.call(obj, datos);
+    ver(obj);
+})()
