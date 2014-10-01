824cb10be2d7a14e919e99cfeaadf68c544cef2f
TubalLara <tublargar@gmail.com>
2014-10-01 12:22:24 +0200

    segunda practica javascript

---
+++ WebApplication1/scripts/funciones.js
@@ -0,0 +1,34 @@
+﻿function hola(msg) {
+    alert(msg);
+}
+
+var hola=2 function(msg) {
+    alert(msg)
+}
+
+
+hola("Hola");
+
+hola2("Hola");
+
+var hola3 = hola;
+
+hola3("Hola");
+
+//de aquí para abajo es un ejemplo de la diapositiva 6
+
+var pasar=function(objeto) {
+
+    alert(objeto);
+
+}
+
+var recibir=function(pasado) {
+
+    pasado("Estoy llamando a una función desde otra");
+
+
+}
+
+
+recibir(pasar);
---
+++ WebApplication1/scripts/operacionesfunciones.js
@@ -0,0 +1,19 @@
+﻿function sumar(n1, n2) {
+    this.resultado= n1 + n2
+    return n1 + n2;
+}
+
+(function () {
+    var r = sumar(15, 20)
+
+    alert(r)
+    var obj = {x:22,y:44}; //esto es un objeto xq está entre llaves q son la property y el value de la property(se llama JSON)
+
+    //apply y call
+    var args = [5, 23];
+    var rr = sumar.apply(obj, args); //el apply me sirve para poder cambiar la referencia de this, y para acceder a propiedades de la función que de otras maneras no podría
+    var rrr = sumar.call(obj, 5, 23); //apply y call tienen su utilidad cuando se trabaja con objetos
+
+    alert("Con apply" +rr);
+    alert("Con call" +rrr);
+})();
